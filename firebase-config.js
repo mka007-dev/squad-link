b2a4434668d5d1af93b3ab933a0243024bc24287
mka007-dev <mikefred73@gmail.com>
2026-06-09 02:31:33 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,1 @@
-window.LOBBYRUSH_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBhF1IPaH9VtYIiifV9RmHSN54ZQMn5RKY",
-  authDomain: "squad-link-aa29e.firebaseapp.com",
-  projectId: "squad-link-aa29e",
-  storageBucket: "squad-link-aa29e.firebasestorage.app",
-  messagingSenderId: "980338511143",
-  appId: "1:980338511143:web:bd7d5fbd0dcd1affe8ad02",
-  measurementId: "G-ED46XLGYS3"
-};
 
-window.LOBBYRUSH_FIREBASE_ENABLED = true;
-window.LOBBYRUSH_APP_CHECK_SITE_KEY = "6LeEGfwsAAAAAMGq2P4c1CPZXBm3kqyMFvsKEgV7";
-window.SQUADLINK_FIREBASE_CONFIG = window.LOBBYRUSH_FIREBASE_CONFIG;
-window.SQUADLINK_FIREBASE_ENABLED = window.LOBBYRUSH_FIREBASE_ENABLED;
-window.SQUADLINK_APP_CHECK_SITE_KEY = window.LOBBYRUSH_APP_CHECK_SITE_KEY;
